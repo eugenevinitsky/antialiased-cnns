34294dce01302ce46113c1529e69a369ce4c4067
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2022-01-17 14:15:03 -0800

    add run script for cluster training

--- run_training_cluster.py
+++ run_training_cluster.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python3
-import time
 import os
-import sys
 import argparse
 import pathlib, shutil
 from datetime import datetime
-from subprocess import Popen, DEVNULL
-
-
-class Overrides(object):
-    def __init__(self):
-        self.kvs = dict()
-
-    def add(self, key, values):
-        value = ','.join(str(v) for v in values)
-        assert key not in self.kvs
-        self.kvs[key] = value
-
-    def cmd(self):
-        cmd = []
-        for k, v in self.kvs.items():
-            cmd.append(f'{k}={v}')
-        return cmd
+import submitit
 
 
 def make_code_snap(experiment, code_path, slurm_dir='exp'):
@@ -40,9 +22,7 @@ def make_code_snap(experiment, code_path, slurm_dir='exp'):
         for f in (src_dir / dir).glob(pat):
             shutil.copy(f, dst_dir / f.name)
     
-    dirs_to_copy = ['.', './cfgs/', './cfgs/algo', './algos/', './algos/ppo/', './algos/ppo/ppo_utils',
-                './algos/ppo/r_mappo', './algos/ppo/r_mappo/algorithm', './algos/ppo/utils',
-                './algos/gcsl/', './envs/', './nocturne_utils/', './python/', './scenarios/', './build']
+    dirs_to_copy = ['.']
     src_dir = pathlib.Path(os.path.dirname(os.getcwd()))
     for dir in dirs_to_copy:
         copy_dir(dir, '*.py')
@@ -54,38 +34,25 @@ def make_code_snap(experiment, code_path, slurm_dir='exp'):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('experiment', type=str)
-    parser.add_argument('--code_path', default='/checkpoint/eugenevinitsky/nocturne')
+    parser.add_argument('--code_path', default='/checkpoint/eugenevinitsky/aliasedcnns')
     parser.add_argument('--dry', action='store_true')
     args = parser.parse_args()
    
     snap_dir = make_code_snap(args.experiment, args.code_path)
     print(str(snap_dir))
-    overrides = Overrides()
-    overrides.add('hydra/launcher', ['submitit_slurm'])
-    overrides.add('hydra.launcher.partition', ['learnlab'])
-    overrides.add('experiment', [args.experiment])
-    # experiment parameters
-    overrides.add('episode_length', [200])
-    # algo
-    overrides.add('algo', ['ppo'])
-    overrides.add('algo.entropy_coef', [-0.001, 0.0, 0.001])
-    overrides.add('algo.n_rollout_threads', [128])
-    # rewards
-    overrides.add('rew_cfg.goal_achieved_bonus', [10, 50])
-    # misc
-    overrides.add('scenario_path', ['/private/home/eugenevinitsky/Code/nocturne/scenarios/twenty_car_intersection.json'])
-    
-    cmd = ['python', str(snap_dir / 'code' / 'algos' / 'ppo'/ 'nocturne_runner.py'), '-m']
-    print(cmd)
-    cmd += overrides.cmd()
 
-    if args.dry:
-        print(' '.join(cmd))
-    else:
-        env = os.environ.copy()
-        env['PYTHONPATH'] = str(snap_dir / 'code')
-        p = Popen(cmd, env=env)
-        p.communicate()
+    def function_runner(entropy_scaling):
+        run_str = "python main.py --wandb --seed  --multiprocessing-distributed"
+        run_str += f"--entropy-scale {entropy_scaling}"
+        run_function = submitit.helpers.CommandFunction(run_str.split(' '), cwd=snap_dir,
+                env={'PYTHONPATH': str(snap_dir / 'code')})
+        return run_function
+    executor = submitit.AutoExecutor(folder=snap_dir)
+    executor.update_parameters(timeout_min=1440, slurm_partition="learnlab", gpus_per_node=8)
+    jobs = executor.map_array(function_runner, [0.0, 0.01, 0.1])
+    # then you may want to wait until all jobs are completed:
+    outputs = [job.result() for job in jobs]
+    print(outputs)
 
 
 if __name__ == '__main__':
